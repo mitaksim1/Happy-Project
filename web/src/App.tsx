f30c9842e877a7387d6f3ad99118f3419e282815
mitaksim1 <miriamt.simonnet@gmail.com>
2020-10-13 18:18:25 +0200

    Creating and styling home page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,31 @@
 import React from 'react';
+import { FiArrowRight } from 'react-icons/fi';
 
 import './styles/global.css';
+import './styles/pages/landing.css';
+
+import logo from './images/logo.svg';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div id="page-landing">
+      <div className="content-wrapper">
+        <img src={logo} alt="Happy"/>
+
+        <main>
+          <h1>Apportez le bonheur au monde</h1>
+          <p>Visitez des orphelinats et changez la journée de plusieurs enfants</p>
+        </main>
+
+        <div className="location">
+          <strong>Limoges</strong>
+          <span>Haute-Vienne</span>
+        </div>
+
+        <a href="" className="enter-app">
+          <FiArrowRight size={26} color="rgba(0, 0, 0, 0.6)" />
+        </a>
+      </div>
     </div>
   );
 }
