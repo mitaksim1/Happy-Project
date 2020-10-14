9d418afab54eb1763419e29f32691abe84604bf6
mitaksim1 <miriamt.simonnet@gmail.com>
2020-10-14 16:44:58 +0200

    Creating an orphanage ok

--- backend/src/models/Orphanage.ts
+++ backend/src/models/Orphanage.ts
@@ -22,4 +22,7 @@ export default class Orphanage {
 
     @Column()
     opening_hours: string;
+
+    @Column()
+    open_on_weekends: boolean;
 }
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,6 @@
 import express, { response } from 'express';
+import { getRepository } from 'typeorm';
+import Orphanage from './models/Orphanage';
 
 import './database/connection';
 
@@ -6,7 +8,35 @@ const app = express();
 
 app.use(express.json());
 
-app.post('/users/:id', (request, response) => {
+app.post('/orphanages', async(request, response) => {
+    // console.log(request.body);
+    const {
+        name,
+        latitude,
+        longitude,
+        about,
+        instructions,
+        opening_hours,
+        open_on_weekends,
+    } = request.body;
+
+    // Which schema model we want to use
+    const orphanagesRepository = getRepository(Orphanage);
+
+    // Creates an orphanage
+    const orphanage = orphanagesRepository.create({
+        name,
+        latitude,
+        longitude,
+        about,
+        instructions,
+        opening_hours,
+        open_on_weekends,
+    });
+
+    // Saves the created orphanage into the db
+    await orphanagesRepository.save(orphanage);
+
     return response.json({ message: 'Hello World'});
 });
 
