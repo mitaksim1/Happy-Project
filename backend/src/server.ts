71300ce8c63972c076d13245cde79a7950808b11
mitaksim1 <miriamt.simonnet@gmail.com>
2020-10-16 17:29:04 +0200

    Moving use cors to fix the bug cors in the frontend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,8 @@
 import express from 'express';
 import path from 'path';
-import 'express-async-errors';
 import cors from 'cors';
+
+import 'express-async-errors';
 
 import './database/connection';
 
@@ -10,10 +11,10 @@ import errorHandler from './errors/handler';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
 app.use(errorHandler);
-app.use(cors());
 
 app.listen(3333);
