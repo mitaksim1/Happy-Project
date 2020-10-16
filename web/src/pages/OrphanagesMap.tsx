401d2f2bdcd22e9ab78a81172d09c6fe3a83108e
mitaksim1 <miriamt.simonnet@gmail.com>
2020-10-16 14:58:59 +0200

    Map's page donne: marker, link to create an orphanage page, link to show a single orphanage by its id and changing map's api adress from mapbox to openstreetmap

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { FiPlus } from 'react-icons/fi';
-import { Map, TileLayer } from 'react-leaflet';
+import { FiPlus, FiArrowRight } from 'react-icons/fi';
+import { Map, TileLayer, Marker, Popup} from 'react-leaflet';
+import Leaflet from 'leaflet';
 
 import '../styles/pages/orphanages-map.css';
 import 'leaflet/dist/leaflet.css';
 
 import mapMarkerImg from '../images/map-marker.svg';
+
+// Get icon to map
+const mapIcon = Leaflet.icon({
+    iconUrl: mapMarkerImg,
+    iconSize: [58, 68],
+    iconAnchor: [29,68], 
+    popupAnchor: [170, 2]
+})
 
 function OrphanagesMap() {
     return (
@@ -30,10 +39,24 @@ function OrphanagesMap() {
                 zoom={14}
                 style={{ width: '100%', height: '100%' }}
             >
-                <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+                <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" 
+                />
+
+                <Marker
+                    icon={mapIcon}
+                    position={[45.8489856, 1.277952 ]}
+                >
+                    <Popup closeButton={false} minWidth={240} maxWidth={240} className="map-popup">
+                        La maison des enfants
+                        <Link className="map-link" to="orphanages/1">
+                            <FiArrowRight size={20} color="FFF" />
+                        </Link>
+                    </Popup>
+                </Marker>
+
             </Map>
 
-            <Link to="" className="create-orphanage">
+            <Link to="/orphanages/create" className="create-orphanage">
                 <FiPlus size={32} color="FFF" />
             </Link>
         </div>
